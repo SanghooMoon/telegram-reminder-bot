c7fce570e6e652ba009549dc094299749fab331c
SanghooMoon <sanghoo.moon@gmail.com>
2022-10-02 21:01:55 +0900

    Initial commit

---
+++ src/main/java/com/shmoon/telegramreminderbot/TelegramReminderBotApplication.java
@@ -0,0 +1,13 @@
+package com.shmoon.telegramreminderbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramReminderBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramReminderBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shmoon/telegramreminderbot/TelegramReminderBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shmoon.telegramreminderbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramReminderBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
