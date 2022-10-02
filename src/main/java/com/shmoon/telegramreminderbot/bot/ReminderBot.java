cf2cfa96468892c0eb738066783243954fe4bfe0
SanghooMoon <sanghoo.moon@gmail.com>
2022-10-02 23:29:59 +0900

    feat : 내가 보낸 메시지 그대로 응답받기

--- src/main/java/com/shmoon/telegramreminderbot/bot/ReminderBot.java
+++ src/main/java/com/shmoon/telegramreminderbot/bot/ReminderBot.java
@@ -1,10 +1,11 @@
 package com.shmoon.telegramreminderbot.bot;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class ReminderBot extends TelegramLongPollingBot {
@@ -24,6 +25,14 @@ public class ReminderBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        String chatId = String.valueOf(update.getMessage().getChatId());
+        String messageText = update.getMessage().getText();
+
+        try {
+            execute(SendMessage.builder().chatId(chatId).text(messageText).build());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
 
     }
 }
