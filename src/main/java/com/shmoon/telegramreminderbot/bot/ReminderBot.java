924949081dc7df6c4220be16dae06bf079ff85a3
SanghooMoon <sanghoo.moon@gmail.com>
2022-10-03 15:09:32 +0900

    feat : 명령어 사용하여 처리하기
    - /help 명령어 추가 및 기본 세팅

--- src/main/java/com/shmoon/telegramreminderbot/bot/ReminderBot.java
+++ src/main/java/com/shmoon/telegramreminderbot/bot/ReminderBot.java
@@ -1,5 +1,7 @@
 package com.shmoon.telegramreminderbot.bot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -7,8 +9,11 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+
 @Component
 public class ReminderBot extends TelegramLongPollingBot {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Value("${BOT_TOKEN_KEY}")
     private String botToken;
@@ -25,14 +30,47 @@ public class ReminderBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        String chatId = String.valueOf(update.getMessage().getChatId());
-        String messageText = update.getMessage().getText();
-
         try {
-            execute(SendMessage.builder().chatId(chatId).text(messageText).build());
+            handleCommands(update);
         } catch (TelegramApiException e) {
-            e.printStackTrace();
+            logger.info("onUpdateReceived()", e);
         }
 
+    }
+
+    /**
+     * 명령어 핸들링 메서드
+     *
+     * @param update
+     * @throws TelegramApiException
+     */
+    private void handleCommands(Update update) throws TelegramApiException {
+        if (hasCommand(update)) {
+            String chatId = String.valueOf(update.getMessage().getChatId());
+            String messageText = update.getMessage().getText();
+
+            if (messageText.startsWith(Commands.help)) {
+                sendHelpMessage(chatId);
+            }
+        }
+    }
+
+    /**
+     * 명령어(command)인지 확인하는 메서드
+     *
+     * @param update
+     */
+    private boolean hasCommand(Update update) {
+        return update.getMessage().getEntities() != null && "bot_command".equals(update.getMessage().getEntities().get(0).getType());
+    }
+
+    /**
+     * /help 처리 메서드
+     *
+     * @param chatId
+     * @throws TelegramApiException
+     */
+    private void sendHelpMessage(String chatId) throws TelegramApiException {
+        execute(SendMessage.builder().chatId(chatId).text("도움말입니다.").build());
     }
 }
